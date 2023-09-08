b36a2414b1eaddaa59a6c36bec16213ab3577803
programKernel <kapitanaleksy@gmail.com>
2023-09-08 13:42:06 +0200

    application doesnt crash during error 400

--- src/main/java/com/allegromini/front/controller/AuthorizationController.java
+++ src/main/java/com/allegromini/front/controller/AuthorizationController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Controller
 public class AuthorizationController {
@@ -26,7 +27,7 @@ public class AuthorizationController {
         try {
             authorizationService.registerAccount(new AccountDTO(email, password, repeatPassword, tos));
             return "home";
-        } catch(AuthorizationServiceException e) {
+        } catch(AuthorizationServiceException | HttpClientErrorException e) {
             model.addAttribute("errorMessage", e.getMessage());
             return "home";
         }
